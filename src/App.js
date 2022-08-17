a19079e6f9dbb7db4a4050331019d7fc2ce55746
zhangshaohong <zhangshaohong@yizhicloud.cn>
2022-08-17 21:34:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useCallback, useEffect, useState, useRef } from 'react'
 import './App.css'
 import { Waves } from './utils/wave'
 import { Button, Input, Form, InputNumber } from 'antd'
+
 import { toPng } from 'html-to-image'
 import logo from './assets/move.jpg'
 function App() {
+    const { TextArea } = Input
     const layout = {
         labelCol: { span: 8 },
         wrapperCol: { span: 16 }
@@ -12,11 +14,27 @@ function App() {
     let [waveCount, setWaveCount] = useState(5)
     let [form, setForm] = useState({
         title: '输入标题',
-        fontSize: 50
+        fontSize: 50,
+        subtitle: '',
+        subTitleFontSize: 30
     })
 
+    function title() {
+        return { __html: form.title.replace(/\n/g, '<br/>') }
+    }
+
+    function subtitle() {
+        return { __html: form.subtitle.replace(/\n/g, '<br/>') }
+    }
+
     let titleStyle = {
-        fontSize: form.fontSize
+        fontSize: form.fontSize,
+        lineHeight: form.fontSize + 'px'
+    }
+
+    let subTitleStyle = {
+        fontSize: form.subTitleFontSize,
+        lineHeight: form.subTitleFontSize + 'px'
     }
 
     const ref = useRef()
@@ -38,10 +56,6 @@ function App() {
             })
     }, [ref])
 
-    const onFormLayoutChange = () => {
-        console.log('onFormLayoutChange')
-    }
-
     useEffect(() => {
         var waves = new Waves('#holder', {
             fps: false,
@@ -56,12 +70,15 @@ function App() {
                 <div id="holder"></div>
                 <div className="textLayer">
                     <div className="name">「MoveDAO」</div>
-                    <h2 style={titleStyle}>{form.title}</h2>
+                    <div className="textarea">
+                        <h2 style={titleStyle} dangerouslySetInnerHTML={title()}></h2>
+                        <h4 style={subTitleStyle} dangerouslySetInnerHTML={subtitle()}></h4>
+                    </div>
                     <img src={logo} className="logo"></img>
                 </div>
             </div>
             <div className="waveControl">
-                <Input
+                <TextArea
                     value={form.title}
                     onChange={(e) => {
                         setForm({
@@ -69,8 +86,9 @@ function App() {
                             title: e.target.value
                         })
                     }}
-                    addonBefore={<span>标题</span>}
-                ></Input>
+                    rows={4}
+                    placeholder="输入标题"
+                ></TextArea>
                 <InputNumber
                     defaultValue={form.fontSize}
                     onChange={(value) => {
@@ -81,6 +99,31 @@ function App() {
                     }}
                     style={{ width: '100%', marginTop: 20 }}
                     addonBefore={<span>标题字体大小</span>}
+                />
+
+                <TextArea
+                    value={form.subtitle}
+                    onChange={(e) => {
+                        setForm({
+                            ...form,
+                            subtitle: e.target.value
+                        })
+                    }}
+                    rows={4}
+                    style={{ width: '100%', marginTop: 20 }}
+                    placeholder="副标题"
+                ></TextArea>
+
+                <InputNumber
+                    defaultValue={form.subTitleFontSize}
+                    onChange={(value) => {
+                        setForm({
+                            ...form,
+                            subTitleFontSize: value
+                        })
+                    }}
+                    style={{ width: '100%', marginTop: 20 }}
+                    addonBefore={<span>副标题字体大小</span>}
                 />
 
                 <Button onClick={onGenerate} style={{ marginTop: 20 }}>
